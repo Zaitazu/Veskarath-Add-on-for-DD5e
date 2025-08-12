04fcbbd647b98ffc70bfe26e27e4baeb91843f76
Zaitazu <andyschyns@outlook.com>
2025-08-12 12:18:28 +0200

    release 1.0.2

--- world-script.js
+++ world-script.js
@@ -2,14 +2,22 @@ Hooks.once("init", () => {
   CONFIG.DND5E.damageTypes.radium = {
     label: "Radium",      // The displayed name of the damage type
     isPhysical: false,     // Whether this is negated by adamantine/magical/silvered
-    icon: "modules/veskarath-add-on-for-dd5e/img/radia2.svg",             // An svg icon.
-    color: new Color("Chartreuse"),  // The color of the damage type (currently unused).
+    icon: "modules/veskarath-add-on-for-dd5e/img/radia.svg",             // An svg icon.
+    color: new Color(0),  // The color of the damage type (currently unused).
+    reference: ""         // A uuid of a journal entry rules page.
+  };
+
+  CONFIG.DND5E.damageTypes.psionique = {
+    label: "Psionique",      // The displayed name of the damage type
+    isPhysical: false,     // Whether this is negated by adamantine/magical/silvered
+    icon: "modules/veskarath-add-on-for-dd5e/img/brain.svg",             // An svg icon.
+    color: new Color(0),  // The color of the damage type (currently unused).
     reference: ""         // A uuid of a journal entry rules page.
   };
 
   CONFIG.DND5E.spellSchools.radiomantie = {
     label: "Radiomantie",
-    icon: "modules/veskarath-add-on-for-dd5e/img/radia2.svg",
+    icon: "modules/veskarath-add-on-for-dd5e/img/radia.svg",
     fullKey: "radiomantie", // Full key used in enrichers
     reference: "" // UUID of journal entry page for rich tooltips
   };
